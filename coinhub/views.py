f294b9df89f7cd36c1a8ee79f522e5f24b355cff
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-05-19 16:57:00 +0100

    Add base and index template

--- coinhub/views.py
+++ coinhub/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from cloudinary.utils import cloudinary_url
 
-# Create your views here.
+def index(request):
+    public_id = 'vexlzgoneltl2zanknfg'
+    image_url, options = cloudinary_url(public_id)
+    context = {'image_url': image_url}
+    return render(request, 'index.html', context)
