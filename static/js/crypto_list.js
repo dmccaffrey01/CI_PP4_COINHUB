801135cbdad11651d15217fe120920b3331e6a05
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-05-25 12:47:31 +0100

    Add comments to html, css, js files

--- static/js/crypto_list.js
+++ static/js/crypto_list.js
@@ -1,23 +1,33 @@
 const priceChanges = document.querySelectorAll('.crypto-change');
 
-priceChanges.forEach(priceChange => {
-    let num = parseFloat(priceChange.innerText.replace('%', ''));
+/**
+ * Change the color of the price change
+ */
+const changePriceColors = () => {
+  priceChanges.forEach(priceChange => {
+      let num = parseFloat(priceChange.innerText.replace('%', ''));
 
-    if (num > 0 ) {
-        if (priceChange.classList.contains('negative')) {
-            priceChange.classList.remove('negative');
-        }
-        priceChange.classList.add('positive');
-    } else if (num < 0) {
-        if (priceChange.classList.contains('positive')) {
-            priceChange.classList.remove('positive');
-        }
-        priceChange.classList.add('negative');
-    }
-});
+      if (num > 0 ) {
+          if (priceChange.classList.contains('negative')) {
+              priceChange.classList.remove('negative');
+          }
+          priceChange.classList.add('positive');
+      } else if (num < 0) {
+          if (priceChange.classList.contains('positive')) {
+              priceChange.classList.remove('positive');
+          }
+          priceChange.classList.add('negative');
+      }
+  });
+};
+
+changePriceColors();
 
 const prices = document.querySelectorAll('.crypto-price');
 
+/**
+ * Change the price from USD to EUR
+ */
 const priceConversion = async (usdAmount) => {
   try {
     const response = await fetch('https://api.exchangerate-api.com/v4/latest/USD');
@@ -32,6 +42,9 @@ const priceConversion = async (usdAmount) => {
   }
 };
 
+/**
+ * Update the price from USD to EUR
+ */
 const convertPrices = async () => {
   for (const price of prices) {
     const convertedPrice = await priceConversion(parseFloat(price.innerText));
@@ -41,11 +54,18 @@ const convertPrices = async () => {
 
 convertPrices();
 
+/**
+ * Gets the json data stored in the data-sparkline div and returns parsed json data
+ */
 const loadJson = (selector) => {
   const jsonAttribute = document.querySelector(selector).getAttribute('data-sparkline');
   return JSON.parse(jsonAttribute);
 };
 
+/**
+ * Create the sparklines for the crypto list
+ * Gets the data for the sparkline and plots a chart to represent the data
+ */
 const createSparklines = () => {
   var sparklineData = loadJson('#sparkline-data');
 
