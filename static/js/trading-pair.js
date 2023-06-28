1c3172562763cb4407c326c80570d5100ab4f207
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-06-28 23:22:58 +0100

    Fix styling for candlestick chart

--- static/js/trading-pair.js
+++ static/js/trading-pair.js
@@ -49,11 +49,15 @@ const getFormattedChartData = async () => {
         let fD = {};
 
         fD["t"] = convertTime(d["time"]);
-        fD["o"] = d["open"];
+        if (i === 0) {
+            fD["o"] = d["open"];
+        } else {
+            fD["o"] = data[i - 1]["close"];
+        }
         fD["h"] = d["high"];
         fD["l"] = d["low"];
         fD["c"] = d["close"];
-        fD["s"] = [d["open"], d["close"]];
+        fD["s"] = [fD["o"], fD["c"]];
 
         formattedData.push(fD);
     }
@@ -65,6 +69,12 @@ const createChart = async () => {
 
     let formattedData = await getFormattedChartData();
 
+    const dataOptions = {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: false,
+    }
+
     let lows = [];
     let highs = [];
     formattedData.forEach(fD => {
@@ -73,10 +83,15 @@ const createChart = async () => {
     })
     const minValue = Math.min(...lows);
     const maxValue = Math.max(...highs);
-    const extraSpacing = (maxValue - minValue) / 50;
 
-    const suggestedMin = minValue - extraSpacing;
-    const suggestedMax = maxValue + extraSpacing;
+    const suggestedMin = Math.floor(minValue / 20) * 20;
+    const suggestedMax = Math.ceil(maxValue / 20) * 20;
+    console.log(suggestedMin, suggestedMax);
+
+    const lastDate = formattedData[formattedData.length - 1]["t"];
+    const maxDate = new Date(lastDate);
+    maxDate.setMinutes(maxDate.getMinutes() + 5);
+
 
     const data = {
         datasets: [{
@@ -97,13 +112,13 @@ const createChart = async () => {
         }]
       };
 
-      const candlestick = {
+    const candlestick = {
         id: 'candlestick',
         beforeDatasetsDraw(chart, args, pluginOptions) {
             const { ctx, data, chartArea: { top, bottom, left, right, width, height }, scales: {x, y} } = chart;
             ctx.save();
             ctx.lineWidth = 2;
-            ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
+            ctx.strokeStyle = '#333333';
 
             data.datasets[0].data.forEach((datapoint, index) => {
                 ctx.beginPath();
@@ -118,14 +133,68 @@ const createChart = async () => {
             })
         }
     }
+
+    const customScale = {
+        id: 'customScale',
+        afterDatasetsDraw(chart, args, pluginOptions) {
+            const { ctx, data, chartArea: { top, bottom, left, right, width, height }, scales: {x, y} } = chart;
+
+            ctx.save();
+
+            let lastDate;
+
+            const datapointXDiff = x.getPixelForValue(data.datasets[0].data[1].t) - x.getPixelForValue(data.datasets[0].data[0].t);
+
+            data.datasets[0].data.forEach((datapoint, index) => {
+                let date = (datapoint.t).toLocaleString('en', dataOptions);
+                
+                let minute = parseInt(date.slice(-2));
+
+                if (minute % 5 == 0) {
+                    ctx.textAlign = 'center';
+                    ctx.font = 'normal 16px sans-serif';
+                    ctx.fillStyle = '#333333';
+                    ctx.fillText(date, x.getPixelForValue(datapoint.t), bottom + 30)
+                    lastDate = datapoint;
+                }
+
+                if (index == data.datasets[0].data.length - 1) {
+                    date = new Date(datapoint.t);
+                    let minutes = date.getMinutes();
+                    let newMinutes;
+                    if (minutes % 5 == 0) {
+                        newMinutes = minutes + 5;
+                    } else {
+                        newMinutes = Math.ceil(minutes / 5) * 5
+                    }
+                    date.setMinutes(newMinutes);
+                    date = date.toLocaleString('en', dataOptions);
+
+                    let xVal = x.getPixelForValue(lastDate.t) + (datapointXDiff * 5);
+
+                    ctx.textAlign = 'center';
+                    ctx.font = 'normal 16px sans-serif';
+                    ctx.fillStyle = '#333333';
+                    ctx.fillText(date, xVal, bottom + 30)
+                }
+            });
+
+
+        }
+    }
   
-      // config 
-      const config = {
+    // config 
+    const config = {
         type: 'bar',
         data: data,
         options: {
             responsive: true,
             maintainAspectRatio: false,
+            layout: {
+                padding: {
+                    bottom: 30,
+                }
+            },
             plugins: {
                 legend: {
                     display: false,
@@ -140,16 +209,35 @@ const createChart = async () => {
                     type: 'timeseries',
                     time: {
                         unit: 'minute',
-                    }
+                    },
+                    grid: {
+                        display: false,
+                    },
+                    ticks: {
+                        display: false,
+                    },
+                    max: maxDate,
                 },
                 y: {
                     beginAtZero: false,
+                    border: {
+                        display: false,
+                    },
+                    grid: {
+                        borderColor: '#d8d8d8',
+                    },
+                    ticks: { 
+                        font: {
+                            size: 16,
+                        },
+                        color: '#333333',
+                    },
                     min: suggestedMin,
                     max: suggestedMax,
                 }
             }
         },
-        plugins: [candlestick]
+        plugins: [candlestick, customScale]
       };
   
       // render init block
